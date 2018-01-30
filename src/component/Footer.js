e5a3ff6feb769432cea0ed0b0563af4a4434f2f8
lifurongrong <740269496@qq.com>
2018-01-30 12:33:02 +0800

    头部尾部完成

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import "../common/index.less";
-
 
 export default class Footer extends React.Component {
     render() {
         return <ul className="footer">
-            <li><Link to='/home'><i className='iconfont icon-home'></i><span>首页</span></Link></li>
-            <li><Link to='/type'><i className='iconfont icon-weibiaoti1'></i><span>分类</span></Link></li>
-            <li><Link to='/shop'><i className='iconfont icon-xiazai1'></i><span>购物车</span></Link></li>
-            <li><Link to='/community'><i className='iconfont icon-shequ'></i><span>社区</span></Link></li>
-            <li><Link to='/user'><i className='iconfont icon-gerenzhongxin'></i><span>我</span></Link></li>
+            <li><NavLink to='/home'><i className='iconfont icon-iconfonthome0'></i><span>首页</span></NavLink></li>
+            <li><NavLink to='/type'><i className='iconfont icon-weibiaoti1'></i><span>分类</span></NavLink></li>
+            <li><NavLink to='/shop'><i className='iconfont icon-gouwuche-copy'></i><span>购物车</span></NavLink></li>
+            <li><NavLink to='/community'><i className='iconfont icon-shequ'></i><span>社区</span></NavLink></li>
+            <li><NavLink to='/user'><i className='iconfont icon-wode2'></i><span>我</span></NavLink></li>
         </ul>
     }
 }
