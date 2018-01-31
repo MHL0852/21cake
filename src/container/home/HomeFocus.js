52691d76afd994dcf46471ec4d73a48ff3256745
lifurongrong <740269496@qq.com>
2018-01-31 12:00:19 +0800

    首页

---
+++ src/container/home/HomeFocus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export default class HomeFocus extends React.Component {
+    render() {
+        return <div className='box focus'>
+            <h3>廿一客·<span>榜单</span></h3>
+            <ul>
+                {/*<li><img src={a} alt=""/></li>*/}
+                {/*<li><img src={b} alt=""/></li>*/}
+                {/*<li><img src={c} alt=""/></li>*/}
+                {/*<li><img src={d} alt=""/></li>*/}
+            </ul>
+        </div>
+    }
+}
