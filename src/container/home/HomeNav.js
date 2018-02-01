b54d55422644dffa2eb151250841938795dd51b3
lifurongrong <740269496@qq.com>
2018-02-01 11:38:32 +0800

    临时提交

--- src/container/home/HomeNav.js
+++ src/container/home/HomeNav.js
@@ -3,10 +3,11 @@ import {Link} from "react-router-dom";
 
 export default class HomeNav extends React.Component {
     render() {
+        console.log(this.props);
         return <div className='nav'>
             <ul>
                 {this.props.navList.map((item,index)=>(
-                    <li key={index}><Link to='/'><img src={`http://static.21cake.com/${item.img_url}`} alt=""/><span>{item.name}</span></Link></li>
+                    <li key={index}><Link to='/type/cake'><img src={`http://static.21cake.com/${item.img_url}`} alt=""/><span>{item.name}</span></Link></li>
                 ))}
             </ul>
         </div>
