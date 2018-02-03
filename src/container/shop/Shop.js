c567b9dc821b3e4321160d5e5dc2be28beeac90d
mohaila <245756054@qq.com>
2018-02-03 19:05:13 +0800

    服务修改

--- src/container/shop/Shop.js
+++ src/container/shop/Shop.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Shopping_box from './shopping_box_goods';
+import TypeCommon from "../../component/TypeCommon";
+import './index.less'
 
 export default class Shop extends React.Component {
     render() {
-        return <div>Shop</div>
+        return <div className="shopping_container">
+<h5 className="shopping_title">·满150免配送服务费·</h5>
+            <ul className= 'shopping_box_all'>
+                <li className="shopping_box_goods">
+                    <Shopping_box/>
+                    <div className="shopping_box_number">
+                        <a className="shopping_box_minus">
+                            <img src="" alt="减少"/>
+                        </a>
+                        <span>
+                            个数
+                        </span>
+                        <a className="shopping_box_add">
+                            <img src="" alt="增加"/>
+                        </a>
+                    </div>
+                </li>
+                <li className="shopping_box_recommend">
+                    <span className="shopping_recommend_text">搭配商品</span>
+<TypeCommon urlAdrs="list/recommend" location={{pathname:"recommend"}}/>
+                </li>
+            </ul>
+        </div>
     }
 }
---
+++ src/container/shop/shopping_box_goods.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {withRouter, Link} from 'react-router-dom'
+
+@withRouter
+class shopping_box_goods extends React.Component {
+    render() {
+        return <div className="shopping_box_goods_">
+            <Link to="/">
+                <img
+                    src="https://raw.githubusercontent.com/MHL0852/21cake/21cake/server/dist/themes/wap/img/1.00P-full-13.00.jpg"
+                    alt="图片" className="shopping_box_goods_img"/>
+                <h2 className="shopping_box_goods_name">
+                <span className="shopping_box_en_name">
+                英文名字
+                </span>
+                    <span className="shopping_box_name">
+                    中文名字
+                </span>
+                    <span className="shopping_box_size">
+                        规格：个
+                    </span>
+                </h2>
+                <span className="shopping_cart_price">
+商品价格
+            </span>
+            </Link>
+        </div>
+    }
+}
+
+export default shopping_box_goods
