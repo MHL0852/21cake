268184b990c2926b395fbee33d62a237a8dff34d
wangyuan1992 <13315477997@163.com>
2018-02-03 18:54:51 +0800

    购物框完成

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -2,3 +2,4 @@ export const GET_HOME = 'GET_HOME';
 export const GET_DETAIL='GET_DETAIL';
 export const CHANGE_SHOPFLAG='CHANGE_SHOPFLAG';
 export const CHANGE_SHOPDATA='CHANGE_SHOPDATA';
+export const TAB_SHOPBTN='TAB_SHOPBTN';
